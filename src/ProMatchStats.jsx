af82f2c53b0745ee6353757e9b08fad48e9f7e62
Auyezkhan Yernar <elenwarface@gmail.com>
2024-06-17 23:23:53 +0500

    +code ref +displaying wards data on map

--- src/ProMatchStats.jsx
+++ src/ProMatchStats.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   useGetRecentmatchByIdQuery,
   useGetmatchByIdQuery,
@@ -19,126 +19,52 @@ export function ProMatchStats() {
     isLoading: recentmatchesListisLoading,
     error: recentmatchesListerror,
   } = useGetRecentmatchByIdQuery(playerStats.playerStats.account_id);
-  let {
-    data: matchesByPeriodList,
-    isLoading: matchesByPeriodListisLoading,
-    error: matchesByPeriodListerror,
-  } = useGetMatchesByPeriodQuery(playerStats.playerStats.account_id, 7);
-  function lol() {
-    let matches = [];
 
-    fetch("https://api.opendota.com/api/players/1296625/matches?&date=7")
-      .then((res) => {
-        return res.json();
-      })
+  const [totalObsLog, setTotalObsLog] = useState([]);
+  const [totalSenLog, setTotalSenLog] = useState([]);
+
+  //TESTING ARRAY
+  //  const totalObsLog = [{x:130,y:250},{x:230,y:450},{x:430,y:350}]
+
+useEffect(() => {
+    //GET ALL MATCHES LAST 7 DAYS THAT SELECTED PLAYER PLAYED
+    fetch(
+      `https://api.opendota.com/api/players/${playerStats.playerStats.account_id}/matches?&date=7`
+    )
+      .then((res) => res.json())
       .then((data) => {
-        matches = data;
-
-        let matchIDs = matches.map((item) => item.match_id);
-        console.log(playerStats.playerStats.account_id)
-        matchIDs.map((match_id)=>{
-          fetch(`https://api.opendota.com/api/matches/${match_id}`).then((res)=>{
-            return res.json()
-          }).then((data)=>{
-            let matchData = data
-            console.log(matchData.players)
-            let current_player_match_data = matchData.players.find(
-              (p) => p.account_id == playerStats.playerStats.account_id
-            );
-            console.log(current_player_match_data)
-          })
-        })
-        // for (let i = 0; i < matchIDs.length; i++) {
-        
-        //   // 3.3 every player item has obs_log prop. information about wards, the time of their placement and position is stored here
-        //   //    create a list that contains all the information about each ward in each match. It should look like this:
-        //   //    [
-        //   //      { "time": 128, "type": "obs_log", "slot": 0, "x": 158.2, "y": 91.8, "z": 132.2, "entityleft": false, "ehandle": 788732, "key": "[158,92]", "player_slot": 0},
-        //   //      { "time": 129, "type": "obs_log", "slot": 0, "x": 158.2, "y": 91.8, "z": 132.2, "entityleft": false, "ehandle": 788732, "key": "[158,92]", "player_slot": 0},
-        //   //      ...
-        //   //    ]
-        //   for (let i = 0; i < current_player_match_data.obs_log; i++) {
-        //     let item = current_player_match_data.obs_log[i];
-        //     total_obs_log.push(item);
-        //   }
-        //   for (let i = 0; i < current_player_match_data.sen_log; i++) {
-        //     let item = current_player_match_data.sen_log[i];
-        //     total_sen_log.push(item);
-        //   }
-        // }
+        //GET ONLY MATCH'S IDs
+        let matchIDs = data.map((item) => item.match_id);
+        return Promise.all(
+          matchIDs.map((match_id) =>
+            fetch(`https://api.opendota.com/api/matches/${match_id}`).then(
+              (res) => res.json()
+            )
+          )
+        );
+      })
+      .then((matchesData) => {
+        const newTotalObsLog = [];
+        const newTotalSenLog = [];
+        matchesData.forEach((data) => {
+          //GET MATCH INFO OF THE SELECTED PLAYER ONLY
+          let current_player_match_data = data.players.find(
+            (p) => p.account_id == playerStats.playerStats.account_id
+          );
+          //PUSH OBS INFO OF SELECTED PLAYER IN ARRAY
+          current_player_match_data.obs_log.map((i) => {
+            newTotalObsLog.push(i);
+          });
+          //PUSH SENTRY INFO OF SELECTED PLAYER IN ARRAY
+          current_player_match_data.sen_log.map((i) => {
+            newTotalSenLog.push(i);
+          });
+        });
+        setTotalObsLog(newTotalObsLog);
+        setTotalSenLog(newTotalSenLog);
       });
+  }, [playerStats.playerStats.account_id]);
 
-    let matchIDs = matches.map((item) => item.match_id);
-    console.log(matchIDs);
-  }
-
-  const onPeriodChangedCb = (period) => {
-    // 1. fetch matches by period. It can be done using:
-    //    https://api.opendota.com/api/players/1296625/matches?&date={period}
-    //    For example: https://api.opendota.com/api/players/1296625/matches?&date=7 (get all maches for past 7 days)
-    fetch("https://api.opendota.com/api/players/1296625/matches?&date=7").then(
-      (req, res) => {}
-    );
-    const matches = [];
-
-    // 2. create a list of match IDs for this period
-    const matchIDs = matches.map((item) => item.match_id);
-
-    const total_obs_log = [];
-    const total_sen_log = [];
-
-    // 3. get wards data
-    for (let i = 0; i < matchIDs.length; i++) {
-      // 3.1 for every match ID, retrieve it's data from
-      //    https://api.opendota.com/api/matches/{match_id}
-      let matchData = {};
-      // 3.2 every match item (matchData) has players prop:
-      // {
-      //   "version": 21,
-      //   "match_id": 7764358501,
-      //   "draft_timings": [],
-      //   "players": [
-      //     {
-      //       "player_slot": 0,
-      //       "obs_placed": 12,
-      //       "sen_placed": 20,
-      //       ...
-      //       "account_id": 123
-      //     }
-      //   ]
-      //   ...
-      // }
-      //  retrieve player item from matchData.players using selected account_id
-      let current_player_match_data = matchData.players.map(
-        (p) => p.account_id == "account_id"
-      );
-      // 3.3 every player item has obs_log prop. information about wards, the time of their placement and position is stored here
-      //    create a list that contains all the information about each ward in each match. It should look like this:
-      //    [
-      //      { "time": 128, "type": "obs_log", "slot": 0, "x": 158.2, "y": 91.8, "z": 132.2, "entityleft": false, "ehandle": 788732, "key": "[158,92]", "player_slot": 0},
-      //      { "time": 129, "type": "obs_log", "slot": 0, "x": 158.2, "y": 91.8, "z": 132.2, "entityleft": false, "ehandle": 788732, "key": "[158,92]", "player_slot": 0},
-      //      ...
-      //    ]
-      for (let i = 0; i < current_player_match_data.obs_log; i++) {
-        let item = current_player_match_data.obs_log[i];
-        total_obs_log.push(item);
-      }
-      for (let i = 0; i < current_player_match_data.sen_log; i++) {
-        let item = current_player_match_data.sen_log[i];
-        total_sen_log.push(item);
-      }
-    }
-
-    // 4. invoke the dispayHeatMap(wardsData) method
-    dispayHeatMap(total_obs_log, total_sen_log);
-  };
-
-  const dispayHeatMap = (obs, sen) => {
-    // TODO: Implement this
-  };
-
-  let obsLeft = 520;
-  let sentryLeft = 220;
   return (
     <div className="playerStatsComponent">
       <div className="player_info">
@@ -153,13 +79,11 @@ export function ProMatchStats() {
           <h3>Position: {playerStats.playerStats.fantasy_role}</h3>
           <h3>Account ID: {playerStats.playerStats.account_id}</h3>
         </div>
-        <div></div>
       </div>
       <div className="playerStats_nav">
         <button
           onClick={() => {
             setselectedTopic("stats");
-            lol();
           }}
         >
           WARDING HEATMAP
@@ -190,11 +114,13 @@ export function ProMatchStats() {
             <button onClick={() => onPeriodChangedCb(30)}>Month</button>
           </div>
           <div className="dota_map">
-            <div className="obs_ward" style={{ left: `${obsLeft}px` }}></div>
-            <div
-              className="sentry_ward"
-              style={{ left: `${sentryLeft}px` }}
-            ></div>
+            {totalObsLog.map((obs_info, index) => (
+              <div
+                key={index}
+                className="obs_ward"
+                style={{ left: `${obs_info.x}px`, top: `${obs_info.y}px` }}
+              ></div>
+            ))}
           </div>
         </>
       ) : selectedTopic == "matches" ? (
@@ -211,20 +137,14 @@ export function ProMatchStats() {
                 <td>RESULT</td>
                 <td>WARDS PLACED</td>
               </tr>
-              {matchesList.map((match) => {
-                return (
-                  <tr key={match.match_id}>
-                    <td>{match.match_id}</td>
-                    <td>{match.duration}</td>
-                    <td>
-                      {match.radiant_win ? "Radiant Lose" : "Radiant Win"}
-                    </td>
-                    {/* <td>{match.radiant_win}</td> */}
-
-                    <td>{match.average_rank}</td>
-                  </tr>
-                );
-              })}
+              {matchesList.map((match) => (
+                <tr key={match.match_id}>
+                  <td>{match.match_id}</td>
+                  <td>{match.duration}</td>
+                  <td>{match.radiant_win ? "Radiant Lose" : "Radiant Win"}</td>
+                  <td>{match.average_rank}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         )
@@ -241,18 +161,14 @@ export function ProMatchStats() {
               <td>RESULT</td>
               <td>WARDS PLACED</td>
             </tr>
-            {recentmatchesList.map((match) => {
-              return (
-                <tr key={match.match_id}>
-                  <td>{match.match_id}</td>
-                  <td>{match.duration}</td>
-                  <td>{match.radiant_win ? "Radiant Lose" : "Radiant Win"}</td>
-
-                  {/* <td>{match.radiant_win}</td> */}
-                  <td>{match.average_rank}</td>
-                </tr>
-              );
-            })}
+            {recentmatchesList.map((match) => (
+              <tr key={match.match_id}>
+                <td>{match.match_id}</td>
+                <td>{match.duration}</td>
+                <td>{match.radiant_win ? "Radiant Lose" : "Radiant Win"}</td>
+                <td>{match.average_rank}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       )}
