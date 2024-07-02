b64abfbac0ec2b2acef12df4fa8b6099eaeaeaf5
Auyezkhan Yernar <elenwarface@gmail.com>
2024-07-02 22:24:50 +0500

    _

--- src/components/pages/pro-match-stats-page/index.jsx
+++ src/components/pages/pro-match-stats-page/index.jsx
@@ -173,8 +173,8 @@ export function ProMatchStats() {
                     key={index}
                     className="sentry_ward"
                     style={{
-                      left: `${sen_info.x / 1.05}px`,
-                      top: `${(200 - sen_info.y) * 1.6}px`,
+                      left: `${sen_info.x / 1.15}px`,
+                      top: `${(220 - sen_info.y) * 1.9}px`,
                     }}
                   >
                     <div className="info">
